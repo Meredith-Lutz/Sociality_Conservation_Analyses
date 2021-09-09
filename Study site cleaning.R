63226fff41c8fd7360beb0cdcb2be4ba99991e1a
Josephine Hubbard <jahubbard@ucdavis.edu>
2021-09-09 16:59:55 -0700

    study site lists

---
+++ Study site cleaning.R
@@ -0,0 +1,53 @@
+setwd("/Users/josephinehubbard/Desktop/Desktop/Cleaned PREDICT data")
+
+
+SS_001_500old 		<- read.csv("SS_001_500.csv")
+SS_501_1000old 		<- read.csv("SS_501_1000.csv")
+SS_001_500verified 	<- read.csv("SS_001_500_verified.csv")
+SS_501_1000verified <- read.csv("SS_501_1000_verified.csv")
+SS_1001_1500	 	<- read.csv("SS_1001_1500.csv")
+SS_1501_2000	 	<- read.csv("SS_1501_2000.csv")
+SS_2001_2500	 	<- read.csv("SS_2001_2500.csv")
+SS_2501_3000	 	<- read.csv("SS_2501_3000.csv")
+SS_3001_3500	 	<- read.csv("SS_3001_3500.csv")
+SS_3501_4000	 	<- read.csv("SS_3501_4000.csv")
+
+colnames(SS_3501_4000) <- colnames(SS_3001_3500) <- colnames(SS_2501_3000) <- colnames(SS_2001_2500) <- colnames(SS_1501_2000) <- colnames(SS_1001_1500) <- colnames(SS_501_1000old) <- colnames(SS_001_500old)
+
+oldformat_studysite	<- rbind.data.frame(SS_001_500old, SS_501_1000old, SS_1001_1500, SS_1501_2000, SS_2001_2500, SS_2501_3000, SS_3001_3500, SS_3501_4000)
+
+colnames(SS_001_500verified) <- colnames(SS_501_1000verified)
+
+newformat_studysite <- rbind.data.frame(SS_001_500verified, SS_501_1000verified)
+
+verified_article_ids <- unique(newformat_studysite$Article.ID)
+
+oldformat_studysite_notverified <- oldformat_studysite[!oldformat_studysite$Article.ID%in%verified_article_ids,]
+
+install.packages("stringr")
+library("stringr")
+
+study_sites_split <- data.frame(str_split_fixed(as.character(oldformat_studysite_notverified$Location.of.study),",",n=2))
+colnames(study_sites_split) <- c("small_site", "large_site")
+
+#takes info from small study site column and places into large study site column if large site is empty
+for(i in 1:dim(study_sites_split)[1]){
+if(study_sites_split[i,]$large_site==""){
+	study_sites_split[i,]$large_site <- study_sites_split[i,]$small_site
+	study_sites_split[i,]$small_site <- ""
+}}
+head(study_sites_split, 50)
+
+newformat_small_and_large <- newformat_studysite[,c(19,27)]
+
+colnames(study_sites_split) <- colnames(newformat_small_and_large)
+SS_combined <- rbind.data.frame(study_sites_split, newformat_small_and_large)
+
+colnames(SS_combined)
+
+unique_small_sites <- sort(unique(SS_combined$Location.of.small.study.site))
+unique_large_sites <- sort(unique(SS_combined$Location.of.large.study.site))
+
+oldformat_studysite_notverified[oldformat_studysite_notverified$Location.of.study=="Yasuní National Park",]
+
+newformat_studysite[newformat_studysite$Location.of.large.study.site=="Yasuní National Park",]
